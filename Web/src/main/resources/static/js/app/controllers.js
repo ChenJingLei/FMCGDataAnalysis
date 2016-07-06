26d5ffd57d15aeb1b7c0255437ba7bdfc87ed5e0
cjl20 <cjl4565@163.com>
2016-07-06 09:48:06 +0800

    page finish

--- Web/src/main/resources/static/js/app/controllers.js
+++ Web/src/main/resources/static/js/app/controllers.js
@@ -7,13 +7,21 @@ var FMCGSystemControllers = angular.module('FMCGSystemControllers', []);
 FMCGSystemControllers.controller('ProductListCtrl', ['$scope', 'Product',
     function ($scope, Product) {
         $scope.products = Product.query();
+
+        $scope.getLocalTime = function (nS) {
+            return new Date(nS).toLocaleString().replace(/年|月/g, "-").replace("日","");
+        }
     }]
 );
 
 FMCGSystemControllers.controller('ProductDetailCtrl', ['$scope', '$routeParams', 'Product',
-    function ($scope, $routeParams,Product) {
-        $scope.products = Product.get({productId:$routeParams.productId},function (product) {
+    function ($scope, $routeParams, Product) {
+        $scope.products = Product.get({productId: $routeParams.productId}, function (product) {
             $scope.product = product;
+
+            $scope.getLocalTime = function (nS) {
+                return new Date(nS).toLocaleString().replace(/年|月/g, "-").replace("日","");
+            }
         })
     }]
 );
